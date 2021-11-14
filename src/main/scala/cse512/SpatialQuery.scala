4f14bdd84e3e4f19a8bb2f441b945b0f86eba325
Sunaada Hebbar <shebbarm@asu.edu>
2021-11-13 22:17:27 -0700

    - define ST_Contains method

--- src/main/scala/cse512/SpatialQuery.scala
+++ src/main/scala/cse512/SpatialQuery.scala
@@ -3,7 +3,39 @@ package cse512
 import org.apache.spark.sql.SparkSession
 
 object SpatialQuery extends App {
+  /*
+  * Input: queryRectangle:String, pointString:String
+  * Output: Boolean (true or false)
+  * Definition: Parse queryRectangle as x & Y coordinates of two diagonally opposite points
+  *             Parse pointString as X & Y coordinates
+  *             Check whether the queryRectangle fully contains the point considering on-boundary points as well.
+  * Example Inputs: queryRectangle = "-155.940114, 19.081331, -155.618917, 19.5307"
+  *                 pointString = "-88.331492, 32.324142"
+  * */
   def ST_Contains(queryRectangle: String, pointString: String): Boolean = {
+    if(queryRectangle == null || queryRectangle.isEmpty() || pointString == null || pointString.isEmpty())
+      return false
+
+    val rectangleCoordinates = queryRectangle.split(",")
+    if(rectangleCoordinates.length < 4)
+      return false
+
+    val xOfCorner1 = rectangleCoordinates(0).trim.toDouble
+    val yOfCorner1 = rectangleCoordinates(1).trim.toDouble
+    val xOfCorner2 = rectangleCoordinates(2).trim.toDouble
+    val yOfCorner2 = rectangleCoordinates(3).trim.toDouble
+
+    val pointCoordinates = pointString.split(",")
+    if(pointCoordinates.length < 2)
+      return false
+
+    val pointX = pointCoordinates(0).trim.toDouble
+    val pointY = pointCoordinates(1).trim.toDouble
+
+    if(pointX >=  math.min(xOfCorner1, xOfCorner2) && pointX <= math.max(xOfCorner1, xOfCorner2)
+      && pointY >= math.min(yOfCorner1, yOfCorner2) && pointY <= math.max(yOfCorner1, yOfCorner2))
+      return true
+
     return false
   }
 
